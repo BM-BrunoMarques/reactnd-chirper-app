795cae53a67e9d5934f5aedfb535e9cf0ece86c0
BadMannersBM <bruno.fdm93@gmail.com>
2020-09-03 13:47:57 -0700

    First Reducers

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,7 @@
 import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
 import { receiveTweets } from '../actions/tweets'
+import {setAuthedUser} from '../actions/authedUser '
 
 const AUTHED_ID = 'tylermcginnis'
 
@@ -10,6 +11,7 @@ export function handleInitialData() {
       .then({ users, tweets }) => {
         dispatch(receiveUsers(users))
         dispatch(receiveTweets(tweets))
+        dispatch(setAuthedUser(AUTHED_ID))
       })
   }
 }

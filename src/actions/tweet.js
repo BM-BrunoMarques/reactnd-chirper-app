30bfe5060e97670999b8b2e5f72ac279a41af506
BadMannersBM <bruno.fdm93@gmail.com>
2020-09-06 00:34:40 -0700

    big one D:

--- src/actions/tweet.js
+++
@@ -1,8 +0,0 @@
-export const RECEIVE_TWEETS  = 'RECEIVE_TWEETS'
-
-Export function receiveTweets (tweets) {
-  return {
-    type: RECEIVE_TWEETS,
-    tweets,
-  }
-}
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
+import LoadingBar from 'react-redux-loading'
+import NewTweet from './NewTweet'
+import TweetPage from './TweetPage'
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
   render() {
     return (
       <div>
-        Starter Code
+        <LoadingBar />
+        {this.props.loading === true
+          ? null
+          : <TweetPage match={{params: {id: '6h5ims9iks66d4m7kqizmv'}}}/>}
       </div>
     )
   }
 }
 
-export default App
+function mapStateToProps ({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
